be9e4cad584ceb70b007e775b15099bd66beddcd
Kelunie <caleb.cordero1997@gmail.com>
2026-06-22 21:19:03 -0600

    estructura de front y readme

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>Sitio para Actividades Sociales</h1>
+      </header>
+      <main>
+        <p>Bienvenido. Aquí irá la interfaz de actividades.</p>
+      </main>
+    </div>
+  )
+}
