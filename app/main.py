4199e5c3c957abd006834a4896a1f6e769ed93f4
Kelunie <caleb.cordero1997@gmail.com>
2026-06-29 19:55:20 -0600

    update cors

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 # Punto de entrada de la aplicación
 from fastapi import FastAPI, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
 from app.actividades.router import router as actividades_router
 from app.usuarios.router import router as usuarios_router
 from app.database import client
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(actividades_router)
 app.include_router(usuarios_router)
 
